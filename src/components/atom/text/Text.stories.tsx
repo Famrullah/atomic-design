0804a6d83359267b7872cf9e8b77332644a587f3
faisalamrullah <faisal.amrulah@qoala.id>
2022-01-15 22:18:46 +0700

    feat add stories

--- src/components/atom/text/Text.stories.tsx
+++ src/components/atom/text/Text.stories.tsx
@@ -11,12 +11,35 @@ export default {
   component: Text,
 } as ComponentMeta<typeof Text>
 
-const style = {
-  color: '#666',
-}
-
 export const Primary: ComponentStory<typeof Text> = () => (
-  <Text tag="h1" style={style} className="">
-    Heading 1
-  </Text>
+  <>
+    <Text tag="h1" style={{ color: '#666' }} className="">
+      Heading 1
+    </Text>
+    <Text tag="h2" style={{ color: '#666' }} className="">
+      Heading 2
+    </Text>
+    <Text tag="h3" style={{ color: '#666' }} className="">
+      Heading 3
+    </Text>
+    <Text tag="h4" style={{ color: '#666' }} className="">
+      Heading 4
+    </Text>
+    <Text tag="h5" style={{ color: '#666' }} className="">
+      Heading 5
+    </Text>
+    <Text tag="h6" style={{ color: '#666' }} className="">
+      Heading 6
+    </Text>
+    <Text tag="p" style={{ color: '#666' }} className="">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industrys standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset
+      sheets containing Lorem Ipsum passages, and more recently with desktop
+      publishing software like Aldus PageMaker including versions of Lorem Ipsum
+    </Text>
+  </>
 )
